a9135e035fee7295fb5b3f1ceff5d64d454cd0ec
DanielReinagel <daniel.e.reinagel@gmail.com>
2021-10-01 09:52:28 -0700

    added to useForm and took away from CheckoutForm

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -2,8 +2,13 @@ import { useState } from 'react';
 
 export default (initialValues) => {
   const [values, setValues] = useState(initialValues);
+  const [submitted, setSubmitted] = useState(false);
   const handleChanges = event => {
     setValues({...values, [event.target.name]: event.target.value});
   };
-  return([values, handleChanges]);
+  const handleSubmit = event => {
+    event.preventDefault();
+    setSubmitted(true);
+  }
+  return([values, handleChanges, submitted, handleSubmit]);
 }
